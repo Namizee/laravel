65094fc0ebb0b9bdd7cd36b2dcd57129910a838d
kogot_a <kogotko_a@lwo.by>
2022-08-16 22:00:59 +0300

    #1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
+
 class HomeController extends Controller {
 
 	public function index() {
+//		$data = DB::table('country')->get();
+//		$data = DB::table('country')->limit(5)->get();
+//		$data = DB::table('country')->select('Code', 'Name')->limit(5)->get();
+//		$data = DB::table('country')->select('Code', 'Name')->first();
+//		$data = DB::table('country')->select('Code', 'Name')->orderBy('Code', 'desc')->first();
+//		$data = DB::table('city')->select('ID', 'Name')->find(2);
+		$data = DB::table('city')->select('ID', 'Name')->where([
+			['ID', '>','1'],
+			['ID', '<','5']
+		])->get();
+		return dd($data);
 		return view('home', ['res'=> 5, 'name' => 'John']);
 	}
 
